c36e491a880cc4404d9fae16bb71d0856eb02b18
Ramesh-krishnan-v <rameshram7401@gmail.com>
2024-06-09 22:49:30 -0400

    first commit

--- src/index.js
+++ src/index.js
@@ -2,12 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import StarRating from './StarRating'
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <App />
+    <StarRating maxRating={3} size={20} messages={["Terrible", "Bad", "Amzing", ]} defaultRating={2} />
   </React.StrictMode>
 );
 
